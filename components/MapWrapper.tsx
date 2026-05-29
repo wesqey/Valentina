f87c5ae85a29cbe82a3f06299530cf6f1e5339d4
wesqey <soccerwes2003@gmail.com>
2026-05-29 14:43:48 -0500

    zip change

--- components/MapWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const BuildingMap = dynamic(() => import("./BuildingMap"), { ssr: false });
-
-export default function MapWrapper(props: React.ComponentProps<typeof BuildingMap>) {
-  return <BuildingMap {...props} />;
-}
